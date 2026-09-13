d17ade3ccbc1f032d3cdbcc2d4b4fbfc55b17740
Ayush Soni <soni.ayush.2212@gmail.com>
2026-09-13 21:20:23 +0530

    add: home page and basic stylings

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const Home = () => {
   return (
-    <div>
-    Hello World</div>
+    <main>
+      <nav></nav>
+      <div className="mt-20 flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-semibold">Kanban</h1>
+        <p className="text-xl font-medium">Manage your work with ease.</p>
+      </div>
+
+      <div className="mt-20 flex flex-col justify-center items-center">
+        <div>
+          <Link href="/login" className="bg-violet-400 text-white px-4 py-2 rounded">
+            Log In
+          </Link>
+        </div>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
